9040b774fc69ab018993dfd9528e78fd1035c43a
hnawny <teerapat.tt@hnawny.in.th>
2024-12-12 11:00:01 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="container">
+        <header className="App-header">
+          <h1 className="gradient-text">สวัสดีชาวโลก</h1>
+        </header>
+      </div>
+    </div>
+  );
+}
+
+export default App;
